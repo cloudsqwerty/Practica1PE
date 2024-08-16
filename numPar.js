7828955db4c89537c0029b52c2f2db27277875cb
Isaac <yishaq935@outlook.com>
2024-08-16 11:48:30 -0600

    Create numPar.js

---
+++ numPar.js
@@ -0,0 +1,12 @@
+// Para esta funcion tomare como parametro "number"
+
+function ParOImpar(number) {
+    return number % 2 === 0;
+}
+
+// Esta funcion devolvera true si el numero es par y false si el numero es impar
+
+console.log(ParOImpar(7));
+
+
+
